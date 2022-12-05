3b593cc606b832f6ee542fdea0e476878d4e5ebe
neefly <noahe99@gmail.com>
2022-12-05 01:52:02 -0500

    added testbench api

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -3,10 +3,11 @@ import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix = "";
 
-const generateAction = async (req, res) => {
+export default handler = async (req, res) => {
   console.log(`API: ${basePromptPrefix}${req.body.formattedPrompt}`)
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
@@ -18,5 +19,3 @@ const generateAction = async (req, res) => {
   const basePromptOutput = baseCompletion.data.choices.pop();
   res.status(200).json({ output: basePromptOutput });
 };
-
-export default generateAction;
