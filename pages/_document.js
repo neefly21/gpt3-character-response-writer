7600e30b56c5dd88be6dcd65866e13c3e1aa7562
neefly <noahe99@gmail.com>
2022-12-05 22:23:35 -0500

    updated metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Character Writer" key="title"/>
+        <meta property="og:title" content="GPT-3 Writer" key="title"/>
         <meta property="og:description" content="built with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://i.imgur.com/GrGL7Dl.png"
+          content="https://i.imgur.com/tLUKefK.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
